77b0795707d3dcc7979682385799e2644b21dccf
Yashovardhan Singh <yashovardhansinghrajput@gmail.com>
2020-11-22 19:20:39 +0530

    Connect Database

--- main/views.py
+++ main/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+import psycopg2
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    """ Fetch the Data Base """
+    conn = psycopg2.connect(database="d7vu2qberk3khb", user="khvziiwdidbcus",
+                                    password="a30b1259b978d1ef24ae8bd6c9d942f3cf951062f4ccd7136aa785499f10b3cd", host="ec2-54-152-40-168.compute-1.amazonaws.com", port="5432")
+    cursor = conn.cursor()
+    postgreSQL_select_Query = "select * from product_details"
+    cursor.execute(postgreSQL_select_Query)
+    result = cursor.fetchall()
+    return render(request, "DB.html", result)
